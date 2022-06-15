450d452f49696699a3261fc7bf5a701eae912fe3
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-06-15 12:46:08 +0600

    add db password

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const dbConn = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
+  pass: process.env.DB_PASS,
 });
 
 module.exports = dbConn;
